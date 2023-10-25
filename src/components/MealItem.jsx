73cefa4a74cb6622a7c2fdc90cd408feeabf41b9
Devacius <devaciusaboveall789456@gmail.com>
2023-10-26 03:28:27 +0530

    first commit

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+
+import { currencyFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+import CartContext from '../store/CartContext.jsx';
+
+export default function MealItem({ meal }) {
+  const cartCtx = useContext(CartContext);
+
+  function handleAddMealToCart() {
+    cartCtx.addItem(meal);
+  }
+  let Carthead = 'Add to Cart';
+  
+  if(cartCtx.items.itemquantity > 0){
+    Carthead = cartCtx.items.itemquantity + ' in Cart';
+  }
+  
+  return (
+    <li className="meal-item">
+      <article>
+        <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+        <div>
+          <h3>{meal.name}</h3>
+          <p className="meal-item-price">
+            {currencyFormatter.format(meal.price)}
+          </p>
+          <p className="meal-item-description">{meal.description}</p>
+        </div>
+        <p className="meal-item-actions">
+          <Button onClick={handleAddMealToCart}>{Carthead}</Button>
+        </p>
+      </article>
+    </li>
+  );
+}
